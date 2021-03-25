bb6a70bd2a44c4bb9b5a6d7b2597339cbd7ff9f6
yoyoyachu <ryachna@gmail.com>
2021-03-24 21:44:33 -0400

    rock paper scissors game

---
+++ rockPaperScissors/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>yachna rana</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to  Rock Paper Scissors</h1>
+<p>
+
+<a class="btn btn-info" href="login.php">Please Log In</a>
+</p>
+</div>
+</body>
+
